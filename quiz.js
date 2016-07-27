b7e68c25123ea30004e5f6aeafc05103e35f26c9
Dayne Wright <daynewr@gmail.com>
2016-07-26 22:42:40 -0500

    added json and XHR initial logic

---
+++ quiz.js
@@ -0,0 +1,27 @@
+
+var Carlot = (function(carlot){
+
+  var row = document.querySelector('div[class="row"]');
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'inventory.json');
+  xhr.addEventListener('load', didLoadData);
+  xhr.send();
+
+  function didLoadData () {
+    var cars = JSON.parse(xhr.responseText).cars;
+
+    cars.forEach(function (car) {
+      row.innerHTML += `
+        <div class="col-md-4 center-box">
+          <h2>${car.make}</h2>
+          <h1>${car.model}</h1>
+          <h4>${car.year}</h4>
+          <h2>$${car.price}</h2>
+          <p>${car.description}</p>
+          ${car.purchased ? '<h4 class="btn btn-danger" disabled> Sorry..sold. :(</h4>' : '<h4 class="btn btn-success">For SALE!</h4>'}
+        </div>`
+    })
+  }
+
+})(Carlot || {})
