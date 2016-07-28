9108f7cfa76b1cdb9c0ab4d471f6fe9f9e4782f2
Dayne Wright <daynewr@gmail.com>
2016-07-28 14:55:16 -0500

    updated to keep 3 in a row

--- quiz.js
+++ quiz.js
@@ -1,21 +1,28 @@
 
 
 (function(){
-  var row = document.querySelector('div[class="row"]');
+  var addtoDOM = document.querySelector('.cars');
 
   function populatePage(cars) {
+
     // Loop over the inventory and populate the page
-    cars.forEach(function(car) {
-      row.innerHTML += `
-        <div class="col-md-4 center-box car-block" style="border: 3px solid ${car.color};">
-          <h2>${car.make}</h2>
-          <h1>${car.model}</h1>
-          <h4>${car.year}</h4>
-          <h4 style="color: ${car.color};">${car.color}</h4>
-          <h2>$${car.price}</h2>
-          <p>${car.description}</p>
-          ${car.purchased ? '<h4 class="btn btn-danger" disabled> Sorry..sold. :(</h4>' : '<h4 class="btn btn-success">For SALE!</h4>'}
-        </div>`;
+    cars.forEach(function(car, index) {
+      if(index % 3 === 0){
+        addtoDOM.innerHTML += '<div class="row">';
+      }
+        addtoDOM.innerHTML += `
+          <div class="col-md-4 center-box car-block" style="border: 3px solid ${car.color};">
+            <h2>${car.make}</h2>
+            <h1>${car.model}</h1>
+            <h4>${car.year}</h4>
+            <h4 style="color: ${car.color};">${car.color}</h4>
+            <h2>$${car.price}</h2>
+            <p>${car.description}</p>
+            ${car.purchased ? '<h4 class="btn btn-danger" disabled> Sorry..sold. :(</h4>' : '<h4 class="btn btn-success">For SALE!</h4>'}
+          </div>`;
+      if(index % 3 === 0){
+        addtoDOM.innerHTML += '</div>';
+      }
     })
 
     // Now that the DOM is loaded, establish all the event listeners needed
